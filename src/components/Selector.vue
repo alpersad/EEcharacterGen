c6d7479a53647737499ab8ead99651a8460fd6aa
alpersad <cybernorp@gmail.com>
2021-09-03 17:30:17 -0400

    added character selector component

---
+++ src/components/Selector.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="w-screen">
+    Character Select
+    <div class="flex justify-center ">
+      <select
+        class="border-2 border-gray-900 rounded-md"
+        name="character"
+        id="character"
+        v-model="character"
+      >
+        <option :value="null" disabled>Select Character Class</option>
+        <option
+          v-for="character in characters"
+          :key="character"
+          :value="character"
+          >{{ character }}</option
+        >
+      </select>
+    </div>
+    <div class="flex justify-center mt-5">
+      <button
+        v-on:click="emit()"
+        class="border-2 border-blue-900 rounded-md bg-blue-700 text-blue-50 text p-2 hover:bg-blue-600 hover:text-blue-50"
+      >
+        Generate Character
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["character-gen"],
+  data() {
+    return {
+      character: null,
+      characters: [
+        "Bodyguard",
+        "Criminal",
+        "Doctor",
+        "Explorer",
+        "Mercernary",
+        "Mystic",
+        "Occultist",
+        "Spook"
+      ]
+    };
+  },
+  methods: {
+    emit() {
+      if (this.character == null) {
+        return;
+      }
+      this.$emit("character-gen", this.character);
+    }
+  }
+};
+</script>
+
+<style></style>
